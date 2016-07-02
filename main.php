c94a127a840672ca7c865010e38d9bca3ba4ad80
rodolfobandeira <contato@rodolfobandeira.com>
2016-07-01 21:10:57 -0400

    Layout changes

--- main.php
+++ main.php
@@ -5,11 +5,10 @@
             <p class="text-justify">A primeira vers&atilde;o do sistema operacional OpenBSD data do inicio do ano de 1995 e iniciou como um fork do NetBSD; outro sistema operacional da mesma linhagem do Unix de Berkeley. Dentre alguns dos esfor&ccedil;os desempenhados pelo projeto, vemos seguran&ccedil;a, simplicidade e portabilidade.</p>
             <p class="text-justify">Outros projetos tamb&eacute;m s&atilde;o hospedados e mantidos pelos desenvolvedores do OpenBSD. Alguns desses projetos s&atilde;o: LibreSSL, OpenSSH, OpenBGPd e OpenNTPd.</p>
         </div>
-    </div> <!-- div content -->
-    
-     <div class="row">
-         <hr />
-         <div class="col-lg-12">
+        
+        <div class="row">
+            <hr />
+            <div class="col-lg-12">
              <p>
                 <h2 class="text-center">
                     <a name="download"></a>
@@ -30,41 +29,41 @@
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
              </p>
              <hr />
-         </div>
-     </div>
-
-
-      <div class="row marketing">
-        <div class="col-lg-6">
-            <h4>IRC</h4>
-            <p>
-              Venha bater um papo conosco no canal <a href="http://webchat.freenode.net?randomnick=1&channels=%23openbsd-br&uio=d4" target="_blank">#openbsd-br</a> na
-              FreeNode.
-            </p>
-
-            <h4>GitHub</h4>
-            <p>
-              Colabore com este site ou algum projeto OpenBSD. Visite nosso
-              <a href="https://github.com/openbsd-br/">Github</a>.
-            </p>
-
-            <h4>Lista de Discuss&atilde;o</h4>
-            <p>
-                Temos uma lista de discuss&atilde;o para atender a comunidade Brasileira. Para maiores
-                informações <a href="http://openbsd-br.org/mailman/listinfo/lista">clique aqui.</a>
-            </p>
-
-            <h4>Servidores Espelho</h4>
-            <p>
-                Deseja ser um mirror no Brasil? <a href="#">Clique aqui</a> para maiores informações.
-            </p>
+            </div>
         </div>
-
-        <div class="col-lg-6">
-
-            <a class="twitter-timeline" data-width="500" data-height="500" data-theme="light" href="https://twitter.com/openbsd_br">
-                Tweets by @openbsd_br</a>
-            <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-
+        
+        <div class="row marketing">
+            <div class="col-lg-6">
+                <h4>IRC</h4>
+                <p>
+                  Venha bater um papo conosco no canal <a href="http://webchat.freenode.net?randomnick=1&channels=%23openbsd-br&uio=d4" target="_blank">#openbsd-br</a> na
+                  FreeNode.
+                </p>
+            
+                <h4>GitHub</h4>
+                <p>
+                  Colabore com este site ou algum projeto OpenBSD. Visite nosso
+                  <a href="https://github.com/openbsd-br/">Github</a>.
+                </p>
+            
+                <h4>Lista de Discuss&atilde;o</h4>
+                <p>
+                    Temos uma lista de discuss&atilde;o para atender a comunidade Brasileira. Para maiores
+                    informações <a href="http://openbsd-br.org/mailman/listinfo/lista">clique aqui.</a>
+                </p>
+            
+                <h4>Servidores Espelho</h4>
+                <p>
+                    Deseja ser um mirror no Brasil? <a href="#">Clique aqui</a> para maiores informações.
+                </p>
+            </div>
+            
+            <div class="col-lg-6">
+            
+                <a class="twitter-timeline" data-width="500" data-height="500" data-theme="light" href="https://twitter.com/openbsd_br">
+                    Tweets by @openbsd_br</a>
+                <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+            
+            </div>
         </div>
-      </div>
+    </div> <!-- div content -->
